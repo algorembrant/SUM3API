1f177a79a8df4ff7b3fee9b07533bbbb9190c5fd
callmerem <nodlehaki@gmail.com>
2026-01-26 07:43:43 +0800

    feat: added pdf_3 and havent cargo clean yet

--- mt5-chart/target/debug/build/ahash-ef963b8e4e14a533/build_script_build-ef963b8e4e14a533.d
+++
@@ -1,5 +0,0 @@
-C:\Users\User\Desktop\VSCode\SUM3API\mt5-chart\target\debug\build\ahash-ef963b8e4e14a533\build_script_build-ef963b8e4e14a533.d: C:\Users\User\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\ahash-0.8.12\build.rs
-
-C:\Users\User\Desktop\VSCode\SUM3API\mt5-chart\target\debug\build\ahash-ef963b8e4e14a533\build_script_build-ef963b8e4e14a533.exe: C:\Users\User\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\ahash-0.8.12\build.rs
-
-C:\Users\User\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\ahash-0.8.12\build.rs:
--- mt5-chart/target/debug/deps/windows_x86_64_msvc-012faa7d37bab357.d
+++
@@ -1,5 +0,0 @@
-C:\Users\User\Desktop\VSCode\SUM3API\mt5-chart\target\debug\deps\windows_x86_64_msvc-012faa7d37bab357.d: C:\Users\User\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows_x86_64_msvc-0.48.5\src\lib.rs
-
-C:\Users\User\Desktop\VSCode\SUM3API\mt5-chart\target\debug\deps\libwindows_x86_64_msvc-012faa7d37bab357.rmeta: C:\Users\User\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows_x86_64_msvc-0.48.5\src\lib.rs
-
-C:\Users\User\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows_x86_64_msvc-0.48.5\src\lib.rs:
